9fcab444e84c418da1e4dd19915369f8b21f6e68
priyankathakur64 <priyanka.6432thakur@gmail.com>
2025-01-22 17:34:03 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const reportRoutes = require("./routes/reportRoutes"); // Import the report route
+const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 const port = 5000;
@@ -10,16 +10,16 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Use the report routes for handling `/api/report/*` requests
-app.use("/api/report", reportRoutes);
-
-// Serve static assets in production
+// Serve the React build folder in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+// Use the report routes
+app.use("/api/report", reportRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
